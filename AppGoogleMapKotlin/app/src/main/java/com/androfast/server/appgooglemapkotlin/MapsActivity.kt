d367fe3631d5064286b0e61c5321b297ac9c351d
AndroFast <cespedessanchezalex@gmail.com>
2018-09-12 15:41:17 -0500

    google map basico en kotlin

---
+++ AppGoogleMapKotlin/app/src/main/java/com/androfast/server/appgooglemapkotlin/MapsActivity.kt
@@ -0,0 +1,36 @@
+package com.androfast.server.appgooglemapkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
+
+    private var mMap: GoogleMap? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Agregamos marcadores en peru
+        val marcador = LatLng(-6.761120, -79.86355)
+        mMap?.let {
+            it.addMarker(MarkerOptions().position(marcador).title("Marcador en Peru"))
+            it.moveCamera(CameraUpdateFactory.newLatLng(marcador))
+        }
+    }
+}
